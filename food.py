38415ef5579393fa623cd3e8c8307b9927bbb701
samman123 <smmathis97@gmail.com>
2025-03-05 11:27:42 -0700

    wanting to add scoreboard for later but if used need to turn dow red and blue colors for background

--- food.py
+++ food.py
@@ -1,2 +1,20 @@
+import pygame
+import random
+import config
+
 class Food(object):
-    pass
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = config.orange
+        self.randomize_position()
+    
+    def randomize_position(self):
+        self.position = (
+            random.randint(0, int(config.Grid_Width) - 1) * config.Grid_Size, 
+            random.randint(0, int(config.Grid_Height) - 1) * config.Grid_Size
+        )
+
+    def draw(self, surface):
+            rect= pygame.Rect((self.position[0], self.position[1]), (config.Grid_Size, config.Grid_Size))
+            pygame.draw.rect(surface, self.color, rect)
+            pygame.draw.rect(surface, config.black, rect, 1)
